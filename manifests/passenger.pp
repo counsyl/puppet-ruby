85f59c455976c29bd46b270fb30f6f111b172776
Justin Bronn <justin@counsyl.com>
2013-09-26 12:33:01 -0700

    Update to latest passenger version, added `$apache_module` variable (because location differs from 3.x to 4.x).

--- manifests/passenger.pp
+++ manifests/passenger.pp
@@ -1,16 +1,26 @@
 # == Class: ruby::passenger
 #
-# This class installs Phusion Passenger, which is used to deploy ruby web
+# This class installs Phusion Passenger, which is used to deploy Ruby web
 # applications.
 #
 class ruby::passenger(
   $package  = 'passenger',
   $provider = 'gem',
-  $version  = '3.0.21',
+  $version  = '4.0.19',
 ) {
   include ruby::rack
   include ruby::rake
+
+  # Root location of where passenger gem is installed.
   $root = "${ruby::gemhome}/passenger-${version}"
+
+  # Locations of shared library changed between 3.x and 4.x.
+  if versioncmp($version, '4.0.0') >= 0 {
+    $apache_module = "${root}/buildout/apache2/mod_passenger.so"
+  } else {
+    $apache_module = "${root}/ext/apache2/mod_passenger.so"
+  }
+
   package { $package:
     ensure   => $version,
     provider => $provider,
